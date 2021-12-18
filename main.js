47e7c5aca12c689665147a2a336a8b488c73d80d
Maksim Prudnikau <maksimpruidnikau@gmail.com>
2021-12-18 21:29:13 +0200

    add Qt app

--- main.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "350px";
-    document.getElementById("main").style.marginLeft = "350px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-}
