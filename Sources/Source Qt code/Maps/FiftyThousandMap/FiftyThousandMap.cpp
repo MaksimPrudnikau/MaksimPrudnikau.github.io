afbc7b727159a11b057b3ff80cd77d22aa722e1d
Maksim Prudnikau <maksimpruidnikau@gmail.com>
2022-04-09 15:36:22 +0300

    Исправил расчет 1:50_000
    Неправильно конвертились русские буквы

--- Sources/Source Qt code/Maps/FiftyThousandMap/FiftyThousandMap.cpp
+++ Sources/Source Qt code/Maps/FiftyThousandMap/FiftyThousandMap.cpp
@@ -3,9 +3,11 @@
 //
 
 #include "FiftyThousandMap.h"
+#include <QString>
 
 std::string FiftyThousandMap::GetNomenclature(Point point, Border borders)
 {
+    auto kek = previous.borders;
     double dx = floor((borders.leftDown.Latitude - previous.borders.leftUp.Latitude).ToGrad() / lengthByLatitude.ToGrad());
     double dy = floor((borders.rightDown.Longitude - previous.borders.leftDown.Longitude).ToGrad() / lengthByLongitude.ToGrad());
     auto length = dx * rowLength - (rowLength - dy);
@@ -41,31 +43,33 @@ Point FiftyThousandMap::GetShift(std::string letter)
     Point point;
    
     int latitude = 0, longitude = 0;
-    const char A = (char)-64;
-    const char B = (char)-63;
-    const char V = (char)-62;
-    const char G = (char)-61;
+    const auto A = QStringLiteral("А");
+    const auto B = QStringLiteral("Б");
+    const auto V = QStringLiteral("В");
+    const auto G = QStringLiteral("Г");
+    const auto input = QString::fromStdString(letter);
 
-    if (letter[0] == A)
+    if (QString::compare(input, A, Qt::CaseInsensitive) == 0)
     {
         latitude = 1;
         longitude = 0;
     }
-    else if (letter[0] == B)
+    else if (QString::compare(input, B, Qt::CaseInsensitive) == 0)
     {
         latitude = 1;
         longitude = 1;
     }
-    else if (letter[0] == V)
+    else if (QString::compare(input, V, Qt::CaseInsensitive) == 0)
     {
         latitude = 0;
         longitude = 0;
     }
-    else if (letter[0] == G)
+    else if (QString::compare(input, G, Qt::CaseInsensitive) == 0)
     {
         latitude = 0;
         longitude = 1;
     }
+
 
     for (size_t i = 0; i < latitude; ++i)
     {
